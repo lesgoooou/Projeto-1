468e1c0be90857519d228d08c490fa7756cd3fc0
BrunoBudan0 <unifbmello@fei.edu.br>
2024-11-19 22:03:55 -0300

    Arquivos do Administrador

--- main_admin.c
+++ main_admin.c
@@ -10,45 +10,71 @@ int main(void) {
     int senha;
     int esc;
 
-    Admin admins[3]; // Array para armazenar os usuários
-    criarAdmin(admins); // Populando o array com os usuários
+    Adm adms[3];
+    criarAdmin(adms);
+    
+   // Nome do arquivo único para investidores
+    const char *arquivo_investidores = "investidores.bin";
 
-    printf("Seja bem vindo, insira seu CPF e sua senha!\n");
-    while(1) {    
+    printf("Seja bem vindo! Insira seu CPF e sua senha para realizar o login.\n");
+
+    while (1) {
         printf("CPF: ");
         scanf("%s", cpf);
         printf("Senha: ");
         scanf("%d", &senha);  
-
         printf("\n");
-        int admin_autenticado = -1;
-        for (int i = 0; i < 10; i++) {
-            if (strcmp(admins[i].cpf, cpf) == 0 && admins[i].senha == senha) {
-                admin_autenticado = i;
+
+        int adm_autenticado = -1;
+
+        // Verifica login nos 3 administradores cadastrados
+        for (int i = 0; i < 3; i++) {  // Corrigido de 10 para 3
+            if (strcmp(adms[i].cpf, cpf) == 0 && adms[i].senha == senha) {
+                adm_autenticado = i;
                 break;
             }
         }
-        if (admin_autenticado != -1) {
-        printf("Acesso permitido!\n");
-        // Loop para o menu após login bem-sucedido
-        }
-        while (1) {
-            menu_admin();
-            printf("Escolha uma opcao: ");
-            scanf("%d", &esc);
-            printf("\n");
 
-            if (esc < 1 || esc > 7) {
+        if (adm_autenticado != -1) {
+            printf("Acesso permitido!\n");
+
+            while (1) {
+                menu1_admin();  // Corrigido para o nome correto
+                printf("Escolha uma opcao: ");
+                scanf("%d", &esc);
+                printf("\n");
+
+                if (esc < 1 || esc > 9) {
                     printf("Escolha uma opcao valida!\n");
-            } else {
-                if (esc == 1) {cadastrar_invest();}
-                else if (esc == 2) {exc_invest(); }
-                else if (esc == 3) { cadastrar_cripto(); }
-                else if (esc == 4) { exc_cripto();}
-                else if (esc == 5) { atualizar_cota(); }
-                else if (esc == 6) { return 0;}
+                } else {
+                    if (esc == 1) { 
+                        cadastrar_investidor(arquivo_investidores); 
+                    } else if (esc == 2) { 
+                        excluir_investidor(arquivo_investidores); 
+                    } else if (esc == 3) { 
+                        cadastrar_cripto();
+                    } else if (esc == 4) { 
+                        exc_cripto();
+                    } else if (esc == 5) { 
+                        consultar_saldo(arquivo_investidores); 
+                    } else if (esc == 6) { 
+                        consultar_extrato(arquivo_investidores); 
+                    } else if (esc == 7) { 
+                        printf("Atualizar Cotacao\n");
+                    } else if (esc == 8) { 
+                        printf("Encerrando...\n");
+                        return 0; 
+                    } else if (esc == 9) {
+                        printf("9. Listar Investidores\n");   
+                        listar_investidores(arquivo_investidores);
+                    }
+                }
             }
-        }    
+
+        } else {
+            printf("CPF ou senha incorretos, tente novamente ou pressione CTRL + C para sair!\n");
+        }
     }
+
     return 0;
 }
